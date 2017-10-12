7981445643125ae493ce21b3bb499a72702b5362
Konrad Malawski <ktoso@kappazuki.local>
2017-10-12 23:25:14 +0900

    updated - works with autoconfigure

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,7 @@
 package com.example.demo;
 
-import akka.actor.ActorSystem;
-import akka.event.Logging;
-import akka.stream.ActorMaterializer;
-import akka.stream.Materializer;
-import akka.stream.javadsl.AsPublisher;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.ReactiveAdapterRegistry;
-import org.springframework.util.Assert;
-
-import static org.springframework.core.ReactiveTypeDescriptor.multiValue;
 
 @SpringBootApplication
 public class DemoApplication {
